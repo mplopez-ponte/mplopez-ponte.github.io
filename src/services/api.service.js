e3b44549107e9d967f5e8cf158f9d93a517fcf1d
Maria Paz Lopez <m.p.lopez.ponte@gmail.com>
2026-04-04 16:38:10 +0200

    Solucionando problemas con los endpoints

--- src/services/api.service.js
+++ src/services/api.service.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: `${import.meta.env.VITE_API_URL}/api`  // ← añadir /api aquí
+  baseURL: `${import.meta.env.VITE_API_URL}/api`
 });
 
 // Interceptor para añadir el token automáticamente
