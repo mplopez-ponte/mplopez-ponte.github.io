b0316a2b45d0ebb241f2000c3379b0e98955311c
Maria Paz Lopez <m.p.lopez.ponte@gmail.com>
2026-04-18 22:40:41 +0200

    Añadiendo el Footer al layout

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,6 @@ import TareasPage from './pages/TareasPage';
 import TareaDetallePage from './pages/TareaDetallePage';
 import PerfilPage from './pages/PerfilPage';
 import Layout from './components/layout/Layout';
-import Footer from './components/layout/Footer';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
@@ -70,7 +69,7 @@ function App() {
           theme="dark"
         />
       </BrowserRouter>
-        <Footer />
+        
     </AuthProvider>
     
   );
