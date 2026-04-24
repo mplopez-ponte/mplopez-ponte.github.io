46f3160d0218d5104a0efb102a23937448469188
Maria Paz Lopez <m.p.lopez.ponte@gmail.com>
2026-04-24 19:25:04 +0200

    Añadiendo el punto de acceso a la aplicación

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import { createRoot } from "react-dom/client";
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from 'App';
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+const root = createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
